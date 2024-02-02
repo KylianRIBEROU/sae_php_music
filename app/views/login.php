247cc15c05d211d11b636c22b75c1079e05ef2ec
KylianRIBEROU <kylian.riberou@gmail.com>
2024-02-02 10:56:42 +0100

    feat(): login register page fonctionnelle avec cryptage mdp

--- app/views/login.php
+++ app/views/login.php
@@ -6,9 +6,33 @@
     <title>Page de connexion</title>
 </head>
 <body>
+<?php 
+use models\Utilisateur;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // on récupère les données du formulaire
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // on vérifie si l'utilisateur existe
+    $user = Utilisateur::checkUtilisateurExiste($username);
+    if ($user) {
+        if (Utilisateur::checkCredentials($user->getNom(), $password)) {
+            session_start();
+            $_SESSION['id'] = $user->getIdU();
+            $_SESSION['loggedin'] = true;
+            // on redirige l'utilisateur vers la page d'accueil
+            header('Location: /');
+            exit;
+        }
+    }
+    // si l'utilisateur n'existe pas ou si le mot de passe est incorrect, on affiche un message d'erreur
+    echo '<p>Identifiant ou mot de passe incorrect.</p>';
+}
+
+?>
 <div>
     <h2>Connexion</h2>
-    <form action="liena mettre" method="post">
+    <form action="/login" method="post">
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" required>
 
@@ -17,7 +41,7 @@
 
         <button type="submit">Se connecter</button>
     </form>
-    <p>Vous n'avez pas de compte ? <a href="inscription.php">Inscrivez-vous ici</a>.</p>
+    <p>Vous n'avez pas de compte ? <a href="/register">Inscrivez-vous ici</a>.</p>
 </div>
 </body>
 </html>
